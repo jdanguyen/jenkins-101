edc0ac03f330ea86a1327860d2fc6a647eb43152
danguyen <danguyen.dev.2001@gmail.com>
2025-08-20 14:09:00 +0700

    just simple hello world

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI
+def main():
+    print("Hello from jenkins-101!")
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+if __name__ == "__main__":
+    main()
